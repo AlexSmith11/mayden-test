83d69c5de5900145f00dfc526369842b3399ccc0
alex.smith <alex.smith@3sidedcube.com>
2022-03-09 20:01:18 +0000

    added initial routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\CartController;
+use App\Http\Controllers\CartItemController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/cart', [CartController::class, 'indexAction']); // contains both list of all items and the total price
+Route::post('/cart/create', [CartController::class, 'createAction']); //? only needed on new user creation, user shouldn't be able to do this manually
+Route::post('/cart/email', [CartController::class, 'emailAction']);
+
+Route::get('/cart/item/{item}', [CartItemController::class, 'indexAction']);
+Route::post('/cart/item', [CartItemController::class, 'createAction']);
+Route::delete('/cart/item/{item}', [CartItemController::class, 'deleteAction']);
+Route::patch('/cart/item/{item}/remove', [CartItemController::class, 'crossAction']);
+Route::patch('/cart/item/{item}/reorder', [CartItemController::class, 'reorderAction']);
+
+Route::post('/user/spending_limit', [ , 'spendingLimitAction']);
+
